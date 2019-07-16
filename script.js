edeb75cd9d8662c5622e53f5571b8421cf3b49ad
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-16 12:06:03 +0200

    Add buttons and more counters

--- script.js
+++ script.js
@@ -13,22 +13,40 @@ var Counter = React.createClass({
 
     decrement: function() {
         this.setState({
-          counter: this.state.counter -1
+            counter: this.state.counter -1
         });
     },
 
     render: function() {
         return React.createElement('div', {},
             React.createElement('span', {}, 'Counter ' + this.state.counter),
-            React.createElement(
-                "button", 
+            React.createElement("button", 
                 {onClick: this.increment}, "+"),
-            React.createElement(
-                "button", 
+            React.createElement("button", 
                 {onClick: this.decrement}, "-"),
-            );
-        }
-    });
+        );
+    }
+});
+
+var moreCounters = React.createClass({
+    render: function() {
+        return (
+            React.createElement("div", {
+                className: "app"
+            },
+            React.createElement("div", {},
+                React.createElement("div", {},
+                    React.createElement(Counter, {})),
+
+                React.createElement("div", {},
+                    React.createElement(Counter, {})),
+
+                React.createElement("div", {},
+                    React.createElement(Counter, {})),
+            ),
+        )
+    )}
+});
 
 var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
